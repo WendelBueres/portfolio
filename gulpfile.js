df489407f50cd931561a1ce0fe7c1a91325cafdb
Wendel Bueres <wendelbueres@gmail.com>
2023-04-13 18:55:36 -0300

    fix: :bug: removido gulp-images

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,16 @@
-import image from 'gulp-image';
-import gulp from 'gulp';
-import inlinesource from 'gulp-inline-source';
-import replace from 'gulp-replace';
+const gulp = require('gulp');
+const inlinesource = require('gulp-inline-source');
+const replace = require('gulp-replace');
 
 gulp.task('default', () => {
     return gulp.src('build/*.html')
         .pipe(replace('.js"></script>', '.js" inline></script>'))
         .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
-        .pipe(replace('.src/assets/.*'))
         .pipe(inlinesource(
             {
                 compress: false,
                 ignore: ['png']
             }
         ))
-        .pipe(image({
-            pngquant: true,
-            optipng: false,
-            zopflipng: true,
-            jpegRecompress: false,
-            mozjpeg: true,
-            gifsicle: true,
-            svgo: true,
-            concurrent: 10,
-            quiet: true // defaults to false
-          }))
         .pipe(gulp.dest('./build'));
 })
